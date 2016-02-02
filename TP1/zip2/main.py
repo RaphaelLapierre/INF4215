e97c01735342cc2b5bcd340686cc016819196e37
Raphael Lapierre <raph.lapierre@gmail.com>
2016-02-02 16:57:01 -0500

    Recherche locale

--- TP1/zip2/main.py
+++ TP1/zip2/main.py
@@ -5,96 +5,97 @@ import matplotlib.pyplot as pyplot
 from matplotlib.pyplot import Figure, subplot
 from smallestenclosingcircle import make_circle
 
-K = 0
-C = 0
-
-positions = []
 
 def search(Positions, k, c):
-    global K
-    global C
-    K = k
-    C = c
+    solution = greedy_solution(Positions, k, c)
+    print(str(cout(solution, k,c)))
+    meilleur = solution
+    T = 500.0
+    theta = T
+    P = 50
+    Kmax = 50
+    alpha = 0.8;
 
-    global positions
-    positions = Positions
+    for i in range(0, Kmax):
+        for j in range(1, P+1):
+            voisin = find_neighbour(solution, Positions)
+            delta = cout(solution, k, c) - cout(voisin, k, c)
+            if CritereMetropolis(delta, theta):
+                print("Nouvelle solution: " + str(cout(voisin, k, c)))
+                solution = voisin
+                if cout(solution, k, c) < cout(meilleur, k , c):
+                    meilleur = solution
+                    print("Nouveau meilleux: " + str(cout(meilleur, k, c)))
+        theta *= alpha
 
-    frontier = set()
-    frontier.add(State({}, Positions))
-    current = None
-    
-    while frontier:
-        current = min(frontier, key=lambda x: x.cost(K,C)) 
-        if current.is_solution():
-            break
-        
-        frontier = frontier.union(current.childs())
-        frontier.remove(current)
-        
-    return current
 
+    return meilleur
+
+def find_neighbour(solution, positions):
+    if len(solution) <= 1:
+        return solution
+
+    antenna = solution[int(random.random() * len(solution))]
+    closest_antenna = min([ant for ant in solution if ant != antenna], key=lambda a : dist_squared(a, antenna))
+    ant1_coverage = [p for p in positions if verify_cover(p, antenna)]
+    ant2_coverage = [p for p in positions if verify_cover(p, closest_antenna)]
+
+    le_point = min(ant2_coverage, key=lambda pos : dist_squared(pos, antenna))
+    ant2_coverage = [p for p in ant2_coverage if p != le_point]
+    ant1_coverage.append(le_point)
+
+    new_ant1 = make_circle(ant1_coverage);
+    new_ant2 = make_circle(ant2_coverage);
+
+    solution = [ant for ant in solution if (ant != antenna and ant != closest_antenna)]
+    if new_ant1:
+        solution.append(new_ant1)
+    if new_ant2:
+        solution.append(new_ant2)
+
+    return solution
+
+def cout(solution, K, C):
+    return sum([K + C * antenna[2]**2 for antenna in solution])
+
+def greedy_solution(remaining_positions, k, c):
+    positions = list(remaining_positions)
+    if not remaining_positions:
+        return
+
+    antennas = [(remaining_positions[0][0], remaining_positions[0][1], 1)]
+    remaining_positions = remaining_positions[1:]
+
+    for pos in remaining_positions:
+        closest_antenna = min(antennas, key=lambda x : dist_squared(pos, (x[0],x[1])))
+        covered_points = [p for p in positions if verify_cover(p, closest_antenna)]
+        covered_points.append(pos)
+        new_antenna = make_circle(covered_points)
+
+        if((k+c*new_antenna[2]**2) > (k+c*closest_antenna[2]**2 + k)):
+            new_antenna = (pos[0], pos[1], 1)
+        else:
+            antennas = [ant for ant in antennas if ant != closest_antenna]
+
+        antennas.append(new_antenna)
+        remaining_positions = [p for p in remaining_positions if p != pos]
+    return antennas
+
+def CritereMetropolis(delta, T):
+    if delta > 0:
+        return True
+
+    random.seed()
+    x = delta/T
+    return math.exp(x) >= random.random()
 
 def dist_squared(pos1, pos2):
     return (pos1[0] - pos2[0]) ** 2 + (pos1[1] - pos2[1]) ** 2
 
 
-class State:
-    antennas = tuple()
-    remaining_positions = tuple()
+def verify_cover(position, antenna):
+    return dist_squared(position, (antenna[0], antenna[1])) <= antenna[2] ** 2
 
-    def __init__(self, antennas, remaining_positions):
-        self.antennas = tuple(antennas)
-        self.remaining_positions = tuple(remaining_positions)
-
-    def cost(self, K, C):
-        return sum([K + C * antenna[2]**2 for antenna in self.antennas])
-
-    def verify_cover(self, position, antenna):
-        return dist_squared(position, (antenna[0], antenna[1])) <= antenna[2] ** 2
-
-    def add_antenna(self, antenna):
-        self.antennas = self.antennas + (antenna, )
-        self.remaining_positions = tuple([pos for pos in self.remaining_positions if not self.verify_cover(pos, antenna)])
-
-    def is_solution(self):
-        return not self.remaining_positions
-
-    def childs(self):
-        children = []
-        if not self.antennas:
-            for pos in positions:
-                child = State(self.antennas, self.remaining_positions)
-                child.add_antenna((pos[0], pos[1], 1))
-                child.remaining_positions = tuple([p for p in child.remaining_positions if p != pos])
-                children.append(child)
-        else:
-            child = State(self.antennas, self.remaining_positions)
-            pos = min(self.remaining_positions, key=lambda p : dist_squared(p, self.antennas[-1]))
-            closest_antenna = min(child.antennas, key=lambda x : dist_squared(pos, (x[0],x[1])))
-            #les points couvert par l'antenne
-            covered_points = [p for p in positions if child.verify_cover(p, closest_antenna)]
-            covered_points.append(pos)
-            new_antenna = make_circle(covered_points)
-            if((K+C*new_antenna[2]**2) > (K+C*closest_antenna[2]**2 + K)):
-               new_antenna = (pos[0], pos[1], 1)
-            else:
-                child.antennas = tuple([ant for ant in child.antennas if ant != closest_antenna])
-            child.add_antenna(new_antenna)
-            #child.remaining_positions = tuple([p for p in child.remaining_positions if p != pos])
-            children.append(child)
-        return children
-
-    def __eq__(self, other):
-        return other and sorted(self.antennas) == sorted(other.antennas)
-
-    def __ne__(self, other):
-        return not self.__eq__(other)
-
-    def __hash__(self, **kwargs):
-        return hash(self.antennas)
-
-    def __str__(self):
-        return str(self.antennas)
 
 def main():
     #points = [(30, 0), (10, 10), (25,20),(20, 30), (30, 40), (40, 50), (30, 99)];
@@ -107,8 +108,7 @@ def main():
     result = search(points, 200, 1)
     temp = time.clock() - before
     print("Temps: " + str(temp))
-    print("Cout: " + str(result.cost(K,C)))
-    for antenna in result.antennas:
+    for antenna in result:
         circle = pyplot.Circle((antenna[0],antenna[1]), antenna[2], color='g', fill=False)
         ax.add_patch(circle)
 
@@ -128,7 +128,6 @@ def random_pos(numPos, max):
     return points
 
 if __name__ == "__main__":
-    for _ in range(0, 10):
         main()
 
 
