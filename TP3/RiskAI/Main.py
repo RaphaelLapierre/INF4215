9de9c50ee2c166efd42f88a730973c7ebd9b108b
Alexandre St-Onge <stonge.alex@gmail.com>
2016-04-10 11:52:18 -0400

    add csv

--- TP3/RiskAI/Main.py
+++ TP3/RiskAI/Main.py
@@ -3,6 +3,8 @@ from Controller import *
 from RandomAI import *
 from CarreRougeAi import CarreRougeAI
 
+import csv
+
 finish = False
 def signal_handler(signal, frame):
     global finish
@@ -15,14 +17,22 @@ ai1 = RandomAI() # agent adverse sans apprentissage machine
 ai2 = CarreRougeAI() # agent adverse aleatoire
 
 nbWinAI2 = 0
-for i in xrange(100):
+winRate = []
+lastTenWin = 0
+for i in xrange(10000):
     if finish:
         break
     controller = Controller("Americas", "Normal", "Random", ai1, ai2)
     winningPlayerIndex = controller.play()
     if winningPlayerIndex == 1:
         nbWinAI2 += 1
+        lastTenWin += 1
+
+    if i%10 == 0:
+        winRate.append(lastTenWin/10.0)
 
 ai2.save()
+writer = csv.writer(open("data.csv", 'wb'), delimiter=';')
+writer.writerow(winRate)
 
 print "Nb win of ai2 : ", nbWinAI2
